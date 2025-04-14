0ccfcd1fd80f29585a35447d4c82c21648015396
jauy2310 <jauy2310@colorado.edu>
2025-04-13 20:56:48 -0400

    Adding WS2812 module outline

---
+++ ws2812/ws2812.c
@@ -0,0 +1,31 @@
+#include "ws2812.h"
+
+// kernel module definitions/macros
+#include <linux/init.h>
+#include <linux/module.h>
+MODULE_LICENSE("Dual BSD/GPL");
+
+/**
+ * hello_init()
+ * 
+ * Testing the load/unload of a module
+ */
+static int hello_init(void) {
+    printk(KERN_ALERT "Hello, world (jauy2310)\n");
+    return 0;
+}
+
+/**
+ * hello_exit()
+ * 
+ * Testing the unload of a module
+ */
+static int hello_exit(void) {
+    printk(KERN_ALERT "Goodbye, cruel world (jauy2310)\n");
+}
+
+/**
+ * module init/exit functions
+ */
+module_init(hello_init);
+module_exit(hello_exit);
---
+++ ws2812/ws2812.h
@@ -0,0 +1,7 @@
+#ifndef _WS2812_H_
+#define _WS2812_H_
+
+static int hello_init();
+static int hello_exit();
+
+# endif /* _WS2812_H_ */
