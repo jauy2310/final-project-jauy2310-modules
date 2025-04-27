0aa7341a790731fd889d00ff75974bc888f5cb96
jauy2310 <jauy2310@colorado.edu>
2025-04-27 17:57:10 -0400

    Created test platform driver

--- platform_device/platform_dev.c
+++ platform_device/platform_dev.c
@@ -1,3 +1,17 @@
+#include "platform_dev.h"
+
+/**************************************************************************************
+ * MODULE GLOBALS
+ **************************************************************************************/
+
+// define device major/minor numbers
+static int ws2812_major = 0;
+static int ws2812_minor = 0;
+
+// define a global reference to device struct
+struct ws2812_dev ws2812_device;
+static dev_t ws2812_devno;
+
 // create file operations table
 static const struct file_operations ws2812_fops = {
     .owner = THIS_MODULE,
@@ -7,10 +21,9 @@ static const struct file_operations ws2812_fops = {
 };
 
 // open firmware match table
-static struct of_device_id ws2812_match_table[] = {
-    {
-        .compatible = serial,
-    },
+static const struct of_device_id ws2812_match_table[] = {
+    { .compatible = "ws2812" },
+    {},
 };
 
 // platform driver structure
@@ -24,8 +37,122 @@ static struct platform_driver ws2812_pdriver = {
     .remove = ws2812_remove,
 };
 
-// register the platform module
-module_platform_driver(ws2812_pdriver);
+/**************************************************************************************
+ * MODULE IMPLEMENTATION
+ **************************************************************************************/
+
+static int ws2812_open(struct inode *inode, struct file *file) {
+    LOG("Device opened.");
+    return 0;
+}
+
+static int ws2812_release(struct inode *inode, struct file *file) {
+    LOG("Device released.");
+    return 0;
+}
+
+static ssize_t ws2812_write(struct file *file, const char __user *buf, size_t len, loff_t *offset) {
+    LOG("WS2812 Write (size: %zu)", len);
+    return len;
+}
+
+/**************************************************************************************
+ * MODULE INIT/EXIT/PROBE/REMOVE FUNCTIONS
+ **************************************************************************************/
+
+// module probe (for platform driver)
+static int ws2812_probe(struct platform_device *pdev) {
+    // function setup
+    LOG("Probing Test Platform Driver.");
+    int retval = 0;
+
+    // create area in memory for the char device
+    retval = alloc_chrdev_region(&ws2812_devno, ws2812_minor, 1, MODULE_NAME);
+    if (retval != 0) {
+        LOGE("Error allocating character device region.");
+        goto cleanup;
+    }
+    ws2812_major = MAJOR(ws2812_devno);
+    memset(&ws2812_device, 0, sizeof(struct ws2812_dev));
+
+    // initialize char device
+    cdev_init(&ws2812_device.cdev, &ws2812_fops);
+    ws2812_device.cdev.owner = THIS_MODULE;
+
+    // add cdev to kernel
+    retval = cdev_add(&ws2812_device.cdev, ws2812_devno, 1);
+    if (retval != 0) {
+        LOGE("Error adding cdev.");
+        goto unregister_chrdev;
+    }
+
+    // create device class
+    ws2812_device.class = class_create(MODULE_NAME);
+    if (IS_ERR(ws2812_device.class)) {
+        retval = PTR_ERR(ws2812_device.class);
+        LOGE("Error creating class.");
+        goto del_cdev;
+    }
+
+    // create device
+    ws2812_device.device = device_create(ws2812_device.class, NULL, ws2812_devno, NULL, MODULE_NAME);
+    if (IS_ERR(ws2812_device.device)) {
+        retval = PTR_ERR(ws2812_device.device);
+        LOGE("Error creating device.");
+        goto destroy_class;
+    }
+
+    // return success
+    return 0;
+
+    // cleanup labels 
+destroy_class:
+    class_destroy(ws2812_device.class);
+del_cdev:
+    cdev_del(&ws2812_device.cdev);
+unregister_chrdev:
+    unregister_chrdev_region(ws2812_devno, 1);
+cleanup:
+    return retval;
+}
+
+// module remove (for platform driver)
+static int ws2812_remove(struct platform_device *pdev) {
+    // function setup
+    LOG("Removing Test Platform Driver.");
+
+    // destroy the device registration
+    device_destroy(ws2812_device.class, ws2812_devno);
+
+    // destroy the class registration
+    class_destroy(ws2812_device.class);
+
+    // delete the cdev
+    cdev_del(&ws2812_device.cdev);
+
+    // unregister the memory region for the char device
+    unregister_chrdev_region(ws2812_devno, 1);
+    
+    // return
+    return 0;
+}
+
+
+// module init
+static int __init ws2812_init(void) {
+    LOG("Loading Test Platform Driver.");
+    return platform_driver_register(&ws2812_pdriver);
+}
+
+// module exit
+static void __exit ws2812_exit(void) {
+    LOG("Unloading Test Platform Driver.");
+    platform_driver_unregister(&ws2812_pdriver);
+}
+
+// register module entry/exit points
+module_init(ws2812_init);
+module_exit(ws2812_exit);
 
 // module information
 MODULE_AUTHOR("Jake Uyechi");
--- platform_device/platform_dev.h
+++ platform_device/platform_dev.h
@@ -1,8 +1,13 @@
-#include <linux/fs.h>
-#include <linux/kernel.h>
-#include <linux/module.h>
-#include <linux/platform_device.h>
-#include <linux/init.h>
+#ifndef _PLATFORM_DEV_H_
+#define _PLATFORM_DEV_H_
+
+#include <linux/fs.h>                   // linux file system
+#include <linux/kernel.h>               // kernel
+#include <linux/module.h>               // generic kernel module
+#include <linux/platform_device.h>      // platform devices
+#include <linux/init.h>                 // init/exit
+#include <linux/cdev.h>                 // cdev
+#include <linux/mod_devicetable.h>      // device tables (associate module to device)
 
 // local includes
 #include "log.h"
@@ -14,8 +19,26 @@ typedef struct led_node {
     uint8_t b;
 } led_node;
 
+// module definitions
 #define MODULE_NAME "ws2812"
+#define MAX_LEDS 100
 
+// platform driver/char device definition
 struct ws2812_dev {
+    // generic device/driver
+    struct cdev cdev;
+    struct class *class;
+    struct device *device;
+
+    // driver-specific
     led_node led_strip[MAX_LEDS];
-}
+};
+
+// function prototypes
+static int ws2812_open(struct inode *inode, struct file *file);
+static int ws2812_release(struct inode *inode, struct file *file);
+static ssize_t ws2812_write(struct file *file, const char __user *buf, size_t len, loff_t *offset);
+static int ws2812_probe(struct platform_device *pdev);
+static int ws2812_remove(struct platform_device *pdev);
+
+#endif /* _PLATFORM_DEV_H_ */
