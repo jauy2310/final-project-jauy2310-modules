86f54a4b43bd9ea24aa8b02fd5dd9093e549c67d
jauy2310 <jauy2310@colorado.edu>
2025-04-28 21:43:53 -0400

    Changing LED Number to test

--- platform_device/registers.h
+++ platform_device/registers.h
@@ -18,7 +18,7 @@
  * MACROS/DEFINES
  **************************************************************************************/
 #define NUM_GPIO_PINS 53
-#define LED_PIN 18
+#define LED_PIN 12
 /**
  * CLOCK/PWM CONFIGURATION
  * 
