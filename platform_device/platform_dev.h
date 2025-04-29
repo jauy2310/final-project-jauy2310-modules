cb5482b7e14297eda450811f367aba62de17bb81
jauy2310 <jauy2310@colorado.edu>
2025-04-28 20:38:15 -0400

    fixing bus

--- platform_device/platform_dev.h
+++ platform_device/platform_dev.h
@@ -1,9 +1,15 @@
 #ifndef _PLATFORM_DEV_H_
 #define _PLATFORM_DEV_H_
 
-#include <linux/module.h>
-#include <linux/platform_device.h>
-#include <linux/fs.h>
+#include <linux/fs.h>                   // linux file system
+#include <linux/kernel.h>               // kernel
+#include <linux/module.h>               // generic kernel module
+#include <linux/platform_device.h>      // platform devices
+#include <linux/init.h>                 // init/exit
+#include <linux/cdev.h>                 // cdev
+#include <linux/mod_devicetable.h>      // device tables (associate module to device)
+#include <linux/device.h>               // device definitions
+
 #include <linux/miscdevice.h>
 #include <linux/uaccess.h>
 
