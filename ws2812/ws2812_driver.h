95539e7919442f5f84e16f7a65147a410c463f28
jauy2310 <jauy2310@colorado.edu>
2025-04-30 01:03:13 -0400

    tryingagain

--- ws2812/ws2812_driver.h
+++ ws2812/ws2812_driver.h
@@ -43,13 +43,12 @@
 #define WS2812_GPIO_PIN                     18
 #define WS2812_MAX_LEDS                     5
 #define WS2812_BITS_PER_LED                 24
-#define WS2812_BUFFER_BITS                  8
 #define WS2812_RESET_LATCH_BITS             100
-#define WS2812_DMA_BUFFER_LEN               WS2812_BUFFER_BITS + (WS2812_MAX_LEDS * WS2812_BITS_PER_LED) + WS2812_RESET_LATCH_BITS
+#define WS2812_DMA_BUFFER_LEN               (WS2812_MAX_LEDS * WS2812_BITS_PER_LED) + WS2812_RESET_LATCH_BITS
 
 // constants
-#define PULSE_BIT_0                         33
-#define PULSE_BIT_1                         67
+#define PULSE_BIT_0                         32
+#define PULSE_BIT_1                         64
 #define DELAY_SHORT                         10
 #define DELAY_LONG                          100
 
@@ -76,7 +75,7 @@
  * 
  * 6. OR'ing these together gives the register's set value (0x00006400)
  */
-#define PWMDIV_REGISTER                     (0x00006500)
+#define PWMDIV_REGISTER                     (0x00006400)
 #define PWMDIV_REGISTER_BREATHE             (0x00180000)
 
 // BCM base address in physical memory
